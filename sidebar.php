36f80d024a34c495a615ab557510efb839c593b7
ohjoycelau <ohjoycelau@gmail.com>
2016-07-18 10:54:02 -0700

    JL20160718 - page template about added and formatted

--- sidebar.php
+++ sidebar.php
@@ -7,20 +7,28 @@
  * @package speakout_s
  */
 
-if ( ! is_active_sidebar( 'mc_sidebar' ) ) {
-	return;
-}
+
+if ( is_post_type_archive( 'masterclass' ) || is_singular( 'masterclass' ) ) :
+
+	// if no active sidebar aka not masterclass
+	if ( ! is_active_sidebar( 'mc_sidebar' ) ) :
+		return;
+	endif;
+
+	// has active sidebar aka masterclass
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
-	<?php if ( is_post_type_archive( 'masterclass' ) || is_singular( 'masterclass' ) ) : ?>
-		<div class="mc-widget">
-			<!-- <h1>Register and add widget area</h1> -->
-			<?php if ( is_active_sidebar( 'mc_sidebar' ) ) : ?>
-				<div id="primary-sidebar" clas="primary-sidebar widget-area" role="complementary" >
-					<?php dynamic_sidebar( 'mc_sidebar' ); ?>
-				</div>
-			<?php endif; ?>
-		</div>
-	<?php endif; ?>
+	
+	<div class="mc-widget">
+		<!-- <h1>Register and add widget area</h1> -->
+		<?php if ( is_active_sidebar( 'mc_sidebar' ) ) : ?>
+			<div id="primary-sidebar" clas="primary-sidebar widget-area" role="complementary" >
+				<?php dynamic_sidebar( 'mc_sidebar' ); ?>
+			</div>
+		<?php endif; ?>
+	</div>
+
 </aside><!-- #secondary -->
+
+<?php endif; ?>
